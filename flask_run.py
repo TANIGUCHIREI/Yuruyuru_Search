86a82bcc934380e6a37a260bb4e3a48e79420ea5
TANIGUCHIREI <taniguchirei0j@gmail.com>
2024-03-06 15:48:06 +0900

    aa

--- flask_run.py
+++ flask_run.py
@@ -1,5 +1,4 @@
 #app.run()を__init__.pyではディレクトリの上下の関係でできなかったので、ここでやってしまう
 from app.__init__ import create_app
-if __name__ == ('__main__'):
-    app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+app = create_app()
+app.run(debug=True, host='0.0.0.0', port=5000)
