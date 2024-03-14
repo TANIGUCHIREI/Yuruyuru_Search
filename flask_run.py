57eece086c88835f64fd3db29fd0d88c2c156c46
DESKTOP-5TID4Q1\tanigutirei <taniguchirei0j@gmail.com>
2024-03-15 00:37:01 +0900

    dbegu=False

--- flask_run.py
+++ flask_run.py
@@ -1,4 +1,4 @@
 #app.run()を__init__.pyではディレクトリの上下の関係でできなかったので、ここでやってしまう
 from app.__init__ import create_app
 app = create_app()
-app.run(debug=True, host='127.0.0.1', port=5000)
+app.run(debug=False, host='127.0.0.1', port=5000)
