59cf736292cf7ce6428a421ef0c3cad72db318c8
DESKTOP-5TID4Q1\tanigutirei <taniguchirei0j@gmail.com>
2024-03-16 11:38:51 +0900

    flask_run.pyを修正

--- app/__init__.py
+++ app/__init__.py
@@ -3,56 +3,56 @@ import os,time
 from flask import Flask
 from flask import render_template
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
-    )
+test_config=None
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
+)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+if test_config is None:
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile('config.py', silent=True)
+else:
+    # load the test config if passed in
+    app.config.from_mapping(test_config)
 
-    # a simple page that says hello
-    @app.route('/test')
-    def test():
-        return 'This is test page'
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    @app.errorhandler(404)
-    def page_not_found(error):
-        return render_template('search/page_not_found.html',full_categories_list=[])
-    
+# a simple page that says hello
+@app.route('/test')
+def test():
+    return 'This is test page'
 
-    from . import db
-    db.init_app(app)
-
-    from . import auth
-    app.register_blueprint(auth.bp)
-
-    from . import search
-    app.register_blueprint(search.bp)
-
-    from . import nav
-    app.register_blueprint(nav.bp)
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('search/page_not_found.html',full_categories_list=[])
 
 
-    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 #https://qiita.com/hiroyuki-inaba/items/6c9db8648240e61d4e80　にかいてあったやる
-    app.config['TEMPLATES_AUTO_RELOAD'] = True #自動的にリロードするようにする
-    # app.config['DEBUG'] =True
-    # # app.debug = True
-    # print("debug mode on !!!!!")
-    @app.context_processor
-    def inject_now():
-        return {'now': lambda: int(time.time())} #ブラウザがキャッシュをしないようにcssなどのurlを変更するための対策用。こう書くことによってjinja2がみることができるようになる
-    return app
+from . import db
+db.init_app(app)
+
+from . import auth
+app.register_blueprint(auth.bp)
+
+from . import search
+app.register_blueprint(search.bp)
+
+from . import nav
+app.register_blueprint(nav.bp)
+
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 #https://qiita.com/hiroyuki-inaba/items/6c9db8648240e61d4e80　にかいてあったやる
+app.config['TEMPLATES_AUTO_RELOAD'] = True #自動的にリロードするようにする
+# app.config['DEBUG'] =True
+# # app.debug = True
+# print("debug mode on !!!!!")
+@app.context_processor
+def inject_now():
+    return {'now': lambda: int(time.time())} #ブラウザがキャッシュをしないようにcssなどのurlを変更するための対策用。こう書くことによってjinja2がみることができるようになる
--- flask_run.py
+++ flask_run.py
@@ -1,4 +1,5 @@
 #app.run()を__init__.pyではディレクトリの上下の関係でできなかったので、ここでやってしまう
-from app.__init__ import create_app
-app = create_app()
-app.run(debug=False, host='127.0.0.1', port=5000)
+#https://qiita.com/Jazuma/items/521cf31538cb618d285a　を参考にしました
+from app.__init__ import app
+if __name__ == "__main__":
+    app.run()
