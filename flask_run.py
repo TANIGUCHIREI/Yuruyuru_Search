4d63b5bd4abc123b4f08300e3a31e9f97ba5ee1c
TANIGUCHIREI <taniguchirei0j@gmail.com>
2024-06-01 15:51:46 +0900

    change .gitignore

--- flask_run.py
+++
@@ -1,6 +0,0 @@
-#app.run()を__init__.pyではディレクトリの上下の関係でできなかったので、ここでやってしまう
-#https://qiita.com/Jazuma/items/521cf31538cb618d285a　を参考にしました
-#結果：__init__.py内でcreate_app()関数を作成していたことが二重に起動してしまうことの原因だった。そのまま直に書く必要があった？
-from app.__init__ import application
-if __name__ == "__main__":
-    application.run()
--- データベース作成/c2_get_html_and_info/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-案","@").replace("作画","@").replace("漫画","@").replace("：","").replace(" ","")
-    # author_text = author_text.split("@")
